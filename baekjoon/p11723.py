32c448e2e41b673b9e63778a4fddcf1365913a73
k-kbk <kkbk0077@gmail.com>
2024-04-11 22:16:48 +0900

    11723

---
+++ baekjoon/p11723.py
@@ -0,0 +1,28 @@
+import sys
+
+m = int(sys.stdin.readline())
+
+s = set()
+
+for i in range(m):
+    line = sys.stdin.readline().strip()
+
+    if line == "all":
+        s = set([i for i in range(1, 21)])
+    if line == "empty":
+        s = set()
+
+    command, n = line.split(" ")
+    n = int(n)
+
+    if command == "add":
+        s.add(n)
+    if command == "remove":
+        s.discard(n)
+    if command == "check":
+        print(1 if n in s else 0)
+    if command == "toggle":
+        if n in s:
+            s.discard(n)
+        else:
+            s.add(n)
