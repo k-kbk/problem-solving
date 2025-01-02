ba3fba109aeb335c396359d230fcd32f6725e24d
k-kbk <kkbk0077@gmail.com>
2025-01-02 21:52:44 +0900

    베스트 앨범

--- java/src/programmers/베스트앨범.java
+++ java/src/programmers/베스트앨범.java
@@ -1,41 +1,76 @@
 package programmers;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 class 베스트앨범 {
 
 	public int[] solution(String[] genres, int[] plays) {
-		Map<String, Integer> totalCountMap = new HashMap<>();
+		Map<String, Integer> genreCountsMap = new HashMap<>();
 		for (int i = 0; i < genres.length; i++) {
-			totalCountMap.put(genres[i], totalCountMap.getOrDefault(genres[i], 0) + plays[i]);
+			genreCountsMap.put(genres[i], genreCountsMap.getOrDefault(genres[i], 0) + plays[i]);
 		}
-		Map<Integer, List<Integer[]>> map = new TreeMap<>(Comparator.reverseOrder());
+		List<Music> musics = new ArrayList<>();
 		for (int i = 0; i < genres.length; i++) {
-			int totalCount = totalCountMap.get(genres[i]);
-			List<Integer[]> list = map.getOrDefault(totalCount, new ArrayList<>());
-			list.add(new Integer[]{plays[i], i});
-			map.put(totalCount, list);
+			int genreCounts = genreCountsMap.get(genres[i]);
+			musics.add(new Music(i, plays[i], genreCounts));
 		}
+		Collections.sort(musics);
 		List<Integer> answer = new ArrayList<>();
-		for (Map.Entry<Integer, List<Integer[]>> entry : map.entrySet()) {
-			List<Integer[]> list = entry.getValue();
-			list.sort((a, b) -> {
-				if (!b[0].equals(a[0])) {
-					return b[0] - a[0];
-				} else {
-					return a[1] - b[1];
-				}
-			});
-			answer.add(list.get(0)[1]);
-			if (list.size() > 1) {
-				answer.add(list.get(1)[1]);
+		int count = 0;
+		int prev = 0;
+		for (Music music : musics) {
+			int curr = music.getGenreCounts();
+			if (curr != prev) {
+				count = 0;
+			} else if (count == 2) {
+				continue;
 			}
+			answer.add(music.getId());
+			count += 1;
+			prev = curr;
 		}
 		return answer.stream().mapToInt(Integer::intValue).toArray();
+	}
+
+	class Music implements Comparable<Music> {
+
+		private final int id;
+		private final int plays;
+		private final int genreCounts;
+
+		Music(int id, int plays, int genreCounts) {
+			this.id = id;
+			this.plays = plays;
+			this.genreCounts = genreCounts;
+		}
+
+		int getId() {
+			return this.id;
+		}
+
+		int getPlays() {
+			return this.plays;
+		}
+
+		int getGenreCounts() {
+			return this.genreCounts;
+		}
+
+		@Override
+		public int compareTo(Music other) {
+			if (this.genreCounts != other.genreCounts) {
+				return Integer.compare(other.genreCounts, this.genreCounts);
+			}
+
+			if (this.plays != other.plays) {
+				return Integer.compare(other.plays, this.plays);
+			}
+
+			return Integer.compare(this.id, other.id);
+		}
 	}
 }
