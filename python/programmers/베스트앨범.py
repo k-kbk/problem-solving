621235368a0d9bbb3e4918cb36a5477c22f2be17
k-kbk <kkbk0077@gmail.com>
2025-04-14 22:08:47 +0900

    formatting

--- python/programmers/베스트앨범.py
+++ python/programmers/베스트앨범.py
@@ -3,13 +3,10 @@ from collections import defaultdict
 def solution(genres, plays):
     genre_total = defaultdict(int)
     genre_songs = defaultdict(list)
-    
     for i, (g, p) in enumerate(zip(genres, plays)):
         genre_total[g] += p
         genre_songs[g].append((p, i))
-    
     sorted_genres = sorted(genre_total.items(), key=lambda x: -x[1])
-    
     answer = list()
     for genre, _ in sorted_genres:
         songs = sorted(genre_songs[genre], key=lambda x: (-x[0], x[1]))
--- python/programmers/체육복.py
+++ python/programmers/체육복.py
@@ -1,9 +1,7 @@
 def solution(n, lost, reserve):
-    
     s = set(lost) & set(reserve)
     l = set(lost) - s
     r = set(reserve) - s
-    
     for k in sorted(r):
         if (k - 1) in l:
             l.remove(k - 1)
