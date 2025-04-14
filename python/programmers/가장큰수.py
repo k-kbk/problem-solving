f3c425b30681c38f7470942974a97f8f2bc59414
k-kbk <kkbk0077@gmail.com>
2025-04-14 22:28:53 +0900

    가장 큰 수

---
+++ python/programmers/가장큰수.py
@@ -0,0 +1,15 @@
+from functools import cmp_to_key
+
+def compare(a, b):
+    if a + b > b + a:
+        return -1
+    elif a + b < b + a:
+        return 1
+    else:
+        return 0
+
+def solution(numbers):
+    ls = list(map(str, numbers))
+    numbers_sorted = sorted(ls, key=cmp_to_key(compare))
+    answer = "".join(numbers_sorted)
+    return "0" if answer[0] == "0" else answer
