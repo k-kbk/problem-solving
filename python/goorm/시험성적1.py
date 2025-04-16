3141c20789f2c733d182ad6067055fb9715c5f0f
k-kbk <kkbk0077@gmail.com>
2025-04-16 21:56:21 +0900

    시험 성적1

---
+++ python/goorm/시험성적1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+a = list(map(int, input().rstrip().split()))
+b = list(map(int, input().rstrip().split()))
+
+for i in range(n):
+    count = 0
+    for j in range(n):
+        if a[i] < a[j] and b[i] < b[j]:
+            count += 1
+    print(count, end=" ")
