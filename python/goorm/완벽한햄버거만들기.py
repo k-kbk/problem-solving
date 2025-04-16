f93d533ea80f6d3182b89d360ccd9ed95aa213e1
k-kbk <kkbk0077@gmail.com>
2025-04-16 22:08:22 +0900

    완벽한 햄버거 만들기

---
+++ python/goorm/완벽한햄버거만들기.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+k = list(map(int, input().rstrip().split()))
+
+is_desc = False
+prev = k[0]
+
+for taste in k[1:]:
+	if is_desc and taste > prev:
+		print(0)
+		break
+	if taste < prev:
+		is_desc = True
+	prev = taste
+else:
+	print(sum(k))
