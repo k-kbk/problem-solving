233945cb10265548570c0d4c0e27cab645382c7f
k-kbk <kkbk0077@gmail.com>
2025-04-18 22:52:04 +0900

    연합

---
+++ python/goorm/연합.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    start, end = map(int, input().split())
+    graph[start].append(end)
+
+def bfs(start, graph, visited):
+	q = deque([start])
+	while q:
+		current = q.popleft()
+		for node in graph[current]:
+			if not visited[node] and current in graph[node]:
+				q.append(node)
+				visited[node] = True
+		
+visited = [False] * (n + 1)
+count = 0
+
+for i in range(1, n + 1):
+	if not visited[i]:
+		bfs(i, graph, visited)
+		count += 1
+		
+print(count)
