ece62fea249206d88fc732f674072a7317bc9448
k-kbk <kkbk0077@gmail.com>
2025-04-19 21:30:40 +0900

    JadenCase 문자열 만들기

---
+++ python/programmers/JadenCase문자열만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    result = []
+    is_start = True
+
+    for ch in s.lower():
+        if is_start:
+            result.append(ch.upper())
+        else:
+            result.append(ch)
+        is_start = ch == ' '
+
+    return ''.join(result)
