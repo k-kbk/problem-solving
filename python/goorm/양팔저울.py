b657760c1491c2e1942a9b69bffd7afa2fdd2215
k-kbk <kkbk0077@gmail.com>
2025-04-19 19:33:49 +0900

    양팔저울

---
+++ python/goorm/양팔저울.py
@@ -0,0 +1,16 @@
+def dfs(i, sum):
+    if i == k:
+        if 0 < sum <= max_value:
+            result.add(sum)
+    else:
+        dfs(i + 1, sum + g_list[i])
+        dfs(i + 1, sum - g_list[i])
+        dfs(i + 1, sum)
+
+k = int(input())
+g_list = list(map(int, input().split()))
+max_value = sum(g_list)
+result = set()
+dfs(0, 0)
+
+print(max_value - len(result))
