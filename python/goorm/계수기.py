c6951c4983f345da16046b96f8156f4136d04c49
k-kbk <kkbk0077@gmail.com>
2025-04-19 17:01:35 +0900

    계수기

---
+++ python/goorm/계수기.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+a = list(map(int, input().rstrip().split()))
+b = list(map(int, input().rstrip().split()))
+k = int(input().rstrip())
+
+i = len(b) - 1
+
+while i >= 0 and k > 0:
+	sum = k + b[i]
+	div = a[i] + 1
+	k = sum // div
+	b[i] = sum % div
+	i -= 1
+
+print("".join(map(str, b)))
