c728c0d27232143fd7978b5f87925bbae8000ad1
k-kbk <kkbk0077@gmail.com>
2025-07-10 13:51:33 +0900

    딱지놀이

---
+++ python/goorm/딱지놀이.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+N = int(input())
+
+for _ in range(N):
+	a = {n: 0 for n in range(4, 0, -1)}
+	for n in map(int, input().split()[1:]):
+		a[n] += 1
+	b = {n: 0 for n in range(4, 0, -1)}
+	for n in map(int, input().split()[1:]):
+		b[n] += 1
+	if a == b:
+		print("D")
+		continue
+	for i in range(4, 0, -1):
+		if a[i] > b[i]:
+			print("A")
+			break
+		if a[i] < b[i]:
+			print("B")
+			break
+print()
