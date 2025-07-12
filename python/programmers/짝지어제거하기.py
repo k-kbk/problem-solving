53ab0c7c9dff2b53c48af4d5389d2c7c135aaad4
k-kbk <kkbk0077@gmail.com>
2025-07-12 21:47:41 +0900

    짝지어 제거하기

---
+++ python/programmers/짝지어제거하기.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(s):
+    stack = deque()
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+
+    return 1 if not stack else 0
