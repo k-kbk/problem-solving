9e4d7a88d24c5a680046999dfb197bbce1e5712e
k-kbk <kkbk0077@gmail.com>
2025-07-17 09:32:18 +0900

    0커플

---
+++ python/goorm/0커플.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+N = int(input())
+S = list(map(int, input().split()))
+s_set = set(S)
+
+print(sum(x for x in s_set if -x not in s_set))
