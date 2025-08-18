b1292568c6262e394238ca5f06247bb5f6aaf3e0
k-kbk <kkbk0077@gmail.com>
2025-08-18 23:27:00 +0900

    구름 찾기 깃발

--- python/goorm/구름찾기깃발.py
+++ python/goorm/구름찾기깃발.py
@@ -16,3 +16,20 @@ for i in range(N):
         scores.append(clouds)
 
 print(scores.count(K))
+
+###########################
+
+N, K = map(int, input().split())
+board = [[0] * N for _ in range(N)]
+directions = [(-1, -1), (-1, -0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+for i in range(N):
+    line = list(map(int, input().split()))
+    for j in range(N):
+        if line[j]:
+            board[i][j] = -1000
+            for dr, dc in directions:
+                if 0 <= i + dr < N and 0 <= j + dc < N:
+                    board[i + dr][j + dc] += 1
+
+print(sum(sub.count(K) for sub in board))
